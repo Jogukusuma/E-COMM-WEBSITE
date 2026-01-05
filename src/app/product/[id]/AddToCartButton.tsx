0cbb3d37c107575a5ba69d2982be0dbfd8cc6510
Jogu Kusuma <jogukusuma@gmail.com>
2026-01-05 09:47:40 +0000

    add to cart option should be little small

--- src/app/product/[id]/AddToCartButton.tsx
+++ src/app/product/[id]/AddToCartButton.tsx
@@ -9,7 +9,7 @@ export default function AddToCartButton({ product }: { product: Product }) {
   const { addToCart } = useCart();
 
   return (
-    <Button size="lg" onClick={() => addToCart(product)}>
+    <Button onClick={() => addToCart(product)}>
       <ShoppingCart className="mr-2 h-5 w-5" />
       Add to Cart
     </Button>
